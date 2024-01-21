bf3e80c8387d5a5879d1b32a5a5bea1f5b844dd5
Jon <jontroxell1981@gmail.com>
2024-01-21 03:39:52 -0500

    Complete Test

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+    const cats = ["Milo", "Otis", "Garfield"];
+
+    beforeEach(function () {
+        cats.length = 0;
+      
+        cats.push("Milo", "Otis", "Garfield");
+      });
+
+      function destructivelyAppendCat(name) {
+        cats.push("Ralph")
+      }
+
+      function destructivelyPrependCat(name) {
+        cats.unshift("Bob")
+      }
+
+      function destructivelyRemoveLastCat() {
+        cats.pop()
+      }
+
+      function destructivelyRemoveFirstCat() {
+        cats.shift()
+      }
+
+      function appendCat(name) {
+        var appendCat = [...cats]
+        appendCat.push("Broom")
+        return appendCat
+      }
+
+      function prependCat(name) {
+        var prependCat = [...cats]
+        prependCat.unshift("Arnold")
+        return prependCat
+      }
+      
+      function removeLastCat() {
+        var removeLastCat = [...cats]
+        removeLastCat.pop()
+        return removeLastCat
+      }
+
+      function removeFirstCat() {
+        var removeFirstCat = [...cats]
+        removeFirstCat.shift()
+        return removeFirstCat
+      }
